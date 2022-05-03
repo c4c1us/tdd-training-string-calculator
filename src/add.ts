d70dd88cdd6b3a896ae9291aa745d187f8bf97c4
Thomas Humphry <t2.humphry@gmail.com>
2022-05-03 16:37:38 +0200

    feat: implements add() function and handle many numbers

--- src/add.ts
+++ src/add.ts
@@ -1,3 +1,14 @@
-export function add(number: string) {
-  return '0';
+export function roundDouble(number: number, digits = 1) {
+  const d = Math.pow(10, Math.abs(digits));
+
+  return Math.round(number * d) / d;
+}
+
+export function add(number: string): string {
+  const result = number
+    .split(',')
+    .map((n) => Number(n))
+    .reduce((prev, curr) => prev + curr);
+
+  return roundDouble(result).toString();
 }
--- test/add.test.ts
+++ test/add.test.ts
@@ -4,4 +4,20 @@ describe('add() function', () => {
   it('should return "0" if param is an empty string', () => {
     expect(add('')).toBe('0');
   });
+
+  it('should return "1" if param is "1"', () => {
+    expect(add('1')).toBe('1');
+  });
+
+  it('should return "1,2" if param is "3"', () => {
+    expect(add('1,2')).toBe('3');
+  });
+
+  it('should return "1.1,2.2" if param is "3.3"', () => {
+    expect(add('1.1,2.2')).toBe('3.3');
+  });
+
+  it('should return "1.1,2.2,7,12.1" if param is "22.4"', () => {
+    expect(add('1.1,2.2,7,12.1')).toBe('22.4');
+  });
 });
